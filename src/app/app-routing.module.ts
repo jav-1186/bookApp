2102c056c0519cc47a6fca93e5c3184fc4e189d6
Jeffrey Veit <jveit1186@gmail.com>
2020-10-21 21:38:00 -0700

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AccountComponent } from './account/account.component';
-import { HomeComponent } from './home/home.component';
+//import { HomeComponent } from './home/home.component';
 import { GoalsComponent } from './goals/goals.component';
 import { LibraryComponent } from './library/library.component';
 import { MainComponent } from "./main/main.component";
@@ -10,7 +10,7 @@ import { MainComponent } from "./main/main.component";
 const routes: Routes = [
     {
       path: ' ',
-      component: HomeComponent
+     // component: HomeComponent
     },
 
     {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { HomeComponent } from './home/home.component';
+//import { HeaderComponent } from './header/header.component';
+//import { FooterComponent } from './footer/footer.component';
+//import { HomeComponent } from './home/home.component';
 import { AccountComponent } from './account/account.component';
 import { GoalsComponent } from './goals/goals.component';
 import { LibraryComponent } from './library/library.component';
@@ -14,9 +14,9 @@ import { MainComponent } from './main/main.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    HomeComponent,
+    //HeaderComponent,
+    //FooterComponent,
+    //HomeComponent,
     AccountComponent,
     GoalsComponent,
     LibraryComponent,

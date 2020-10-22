e4177e4bd11cc8fc8b7dff26e00847e51e978eca
Jeffrey Veit <jveit1186@gmail.com>
2020-10-21 20:53:49 -0700

    added components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,36 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AccountComponent } from './account/account.component';
 import { HomeComponent } from './home/home.component';
+import { GoalsComponent } from './goals/goals.component';
+import { LibraryComponent } from './library/library.component';
+import { MainComponent } from "./main/main.component";
 
 
 const routes: Routes = [
     {
-      path: '',
+      path: ' ',
       component: HomeComponent
+    },
+
+    {
+      path: 'account',
+      component: AccountComponent
+    },
+
+    {
+      path: 'goals',
+      component: GoalsComponent
+    },
+
+    {
+      path: 'library',
+      component: LibraryComponent
+    },
+
+    {
+      path: 'main',
+      component: MainComponent
     }
 
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,21 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
+import { AccountComponent } from './account/account.component';
+import { GoalsComponent } from './goals/goals.component';
+import { LibraryComponent } from './library/library.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    AccountComponent,
+    GoalsComponent,
+    LibraryComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,

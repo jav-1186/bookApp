ceeaca897b892fb7f81362af0794707baa4faef8
LittleCarlito <59811183+LittleCarlito@users.noreply.github.com>
2020-10-26 19:40:22 -0500

    Fixing my mistake

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AccountComponent } from './account/account.component';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './Home/home.component';
 import { GoalsComponent } from './goals/goals.component';
 import { LibraryComponent } from './library/library.component';
 import { MainComponent } from './main/main.component';

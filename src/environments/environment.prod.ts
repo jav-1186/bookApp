78af5f669b234f48bcc6462d5e2d868174760026
schultz900 <brianschultz900@gmail.com>
2020-11-07 22:56:05 +0000

    fixed error (added environment.ts configs to environment.prod.ts)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: false,
+  runningOS: 'Windows',
+    firebaseConfig : {
+      apiKey: 'AIzaSyAHx8fKJjMUNGxdvrtTpkVaw2VE1d6m77Y',
+      authDomain: 'bookapp-93f1b.firebaseapp.com',
+      databaseURL: 'https://bookapp-93f1b.firebaseio.com',
+      projectId: 'bookapp-93f1b',
+      storageBucket: 'bookapp-93f1b.appspot.com',
+      messagingSenderId: '308449960617',
+      appId: '1:308449960617:web:69d58e834c05cab6977fb8'
+
+  }
 };

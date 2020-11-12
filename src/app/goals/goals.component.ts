85efa1a57d07a675b9efebfc33054ed4ad0816fd
Jeffrey Veit <jveit1186@gmail.com>
2020-11-12 15:34:17 -0800

    some goal changes

--- src/app/goals/goals.component.ts
+++ src/app/goals/goals.component.ts
@@ -12,18 +12,44 @@ import { Observable } from "rxjs";
 })
 export class GoalsComponent implements OnInit {
 
+  key = 'items';
+  items;
+  queryField;
+  queryField2;
+  queryField3;
+  goals;
 
   constructor() { }
 
   ngOnInit(): void
-  {
+  {}
+
+  onSubmit(): void{
+
+
+
+      console.log(this.queryField);
+      console.log(this.queryField2);
+      console.log(this.queryField3);
+      this.addGoal(this.queryField);
+      this.addGoal(this.queryField2);
+      this.addGoal(this.queryField3);
+      this.goals = new Array;
+ }
+
+ public addGoal(item): void{
+
+
+  this.goals.push(item);
+
+}
 
 
 
 
-
-
-  }
-
-
+ private clear(): void{
+  this.queryField = '';
+  this.queryField2 = '';
+  this.queryField3 = '';
+}
 }

de10b41d59ea0c5323eeddf9cab3c322b7ce9b3c
LittleCarlito <59811183+LittleCarlito@users.noreply.github.com>
2020-11-16 16:09:42 -0600

    Redoing reverted commits

--- src/app/library/library.component.ts
+++ src/app/library/library.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { FirestoreDataService } from '../services/firestore-data.service';
 import { AuthService } from '../services/auth.service';
 import { Observable } from 'rxjs';
-import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 
 interface Item{
   userId: string;
@@ -17,20 +16,12 @@ interface Item{
 })
 export class LibraryComponent implements OnInit {
 
-  ref: AngularFirestoreCollection<Item>;
   items: Observable<Item[]>;
   library;
 
-  constructor(public dataService: FirestoreDataService, public auth: AuthService, private afs: AngularFirestore) { }
+  constructor(public db: FirestoreDataService, public auth: AuthService) { }
 
   ngOnInit(): void {
-    this.ref = this.afs.collection(this.dataService.getId({userId: this.auth.currentUserId, type: 'libEntry' }));
-    this.items = this.ref.valueChanges();
-    // const libRef = this.dataService.getCollection({userId: this.auth.currentUserId, type: 'libEntry'});
-    // this.library = this.dataService.getLibrary();
-    // console.log(this.library);
+    this.items = this.db.getCollection({userId: this.auth.currentUserId, type: 'libEntry'});
   }
-
-
-
 }

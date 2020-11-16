6c15a65cb7f7128705fc4af0e6ffaf8b4026a0fe
schultz900 <brianschultz900@gmail.com>
2020-11-16 00:56:06 +0000

    account component shows auth'd user

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,3 +1,4 @@
+import { AuthService } from './../services/auth.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountComponent implements OnInit {
 
-  constructor() { }
+  user: { userName, password } = { userName: '', password: '' };
+  newUser: { fullName, userName, password } = { fullName: '', userName: '', password: '' };
+  loggedIn = false;
+
+  constructor(public auth: AuthService) { }
+    login() { this.auth.login(); }
+    logout() { this.auth.logout(); }
+
+    loginAttempt(): void {
+      this.basicLogin(this.user);
+    }
+
+    private clear(): void {
+      this.user = { userName: '', password: '' };
+      this.newUser = { fullName: '', userName: '', password: '' };
+    }
+
+    private basicLogin(basicUser): void{
+      this.auth.basicLogin(basicUser);
+      this.clear();
+    }
 
   ngOnInit(): void {
   }
